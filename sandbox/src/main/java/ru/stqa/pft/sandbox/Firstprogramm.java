d69a970fad416a3995224c88b92ee3e78a8fdf74
MoskalenkoAndrey <alan3341106@gmail.com>
2018-10-28 21:03:48 +0300

    Проект отвязан от репозитория гит

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Firstprogramm.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Firstprogramm.java
@@ -2,7 +2,7 @@ package ru.stqa.pft.sandbox;
 
 public class Firstprogramm {
 
-  public static void main (String [] args) {
-   System.out.println("Hellow, World!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hellow, World!");
+    }
 }
