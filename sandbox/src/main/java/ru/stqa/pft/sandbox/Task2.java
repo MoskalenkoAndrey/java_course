6aff6d9e19b27ede236e662856154812e6ba14d3
MoskalenkoAndrey <alan3341106@gmail.com>
2018-11-22 22:09:18 +0300

    дополнил описание первой части задания

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
@@ -10,8 +10,8 @@ public class Task2 {
         Point p2 = new Point();
         p2.x = 4;
         p2.y = 5;
-        System.out.println("Расстояние между точками равно = " + (distance(p1, p2)));
-        
+        System.out.println("Расстояние между точками равно (первая часть задания) = " + (distance(p1, p2)));
+
     }
     public static double distance(Point p1, Point p2) {
         return Math.sqrt((p2.x * p2.x - p1.x * p1.x) + (p2.y * p2.y - p1.y * p1.y));
