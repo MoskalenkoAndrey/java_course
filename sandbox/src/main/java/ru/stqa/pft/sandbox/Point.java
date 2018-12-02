0657f20f4b58daca057747a22c93f03170657891
MoskalenkoAndrey <alan3341106@gmail.com>
2018-12-02 17:16:58 +0300

    Завершение 2 задания

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,4 +4,17 @@ public class Point {
 
     public double x;
     public double y;
+    public double ax;
+    public double ay;
+
+    public Point (double x,double y,double ax,double ay) {
+        this.x = x;
+        this.y = y;
+        this.ax = ax;
+        this.ay = ay;
+
+    }
+    public double distance() {
+        return Math.sqrt((this.ax * this.ax - this.x * this.x) + (this.ay * this.ay - this.y * this.ay));
+    }
  }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
@@ -3,18 +3,11 @@ package ru.stqa.pft.sandbox;
 public class Task2 {
     public static void main(String[] args) {
 // Создание объектов Point
-        Point p1 = new Point();
-        p1.x = 2;
-        p1.y = 3;
+        Point p = new Point(2,3,4, 6);
 
-        Point p2 = new Point();
-        p2.x = 4;
-        p2.y = 5;
-       System.out.println("Расстояние между точками равно (первая часть задания) = " + (distance(p1, p2)));
+       System.out.println("Расстояние между точками R (" + p.x + ";" + p.y + ") и Т (" + p.ax + ";" + p.ay + ") равно " + (p.distance()));
 
     }
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt((p2.x * p2.x - p1.x * p1.x) + (p2.y * p2.y - p1.y * p1.y));
-            }
+
 }
 
