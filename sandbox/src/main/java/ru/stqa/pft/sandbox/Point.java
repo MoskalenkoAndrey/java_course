74df595e535d36de0ff5fa28a5f6c70284b37c59
MoskalenkoAndrey <alan3341106@gmail.com>
2018-12-07 20:12:20 +0300

    Выполнил второе задание

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -4,17 +4,14 @@ public class Point {
 
     public double x;
     public double y;
-    public double ax;
-    public double ay;
 
-    public Point (double x,double y,double ax,double ay) {
+
+    public Point (double x,double y) {
         this.x = x;
         this.y = y;
-        this.ax = ax;
-        this.ay = ay;
-
     }
-    public double distance() {
-        return Math.sqrt((this.ax * this.ax - this.x * this.x) + (this.ay * this.ay - this.y * this.ay));
+
+    public double distance(double x, double y) {
+        return Math.sqrt((x * x - this.x * this.x) + ( y * y - this.y * this.y));
     }
  }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
@@ -3,9 +3,11 @@ package ru.stqa.pft.sandbox;
 public class Task2 {
     public static void main(String[] args) {
 // Создание объектов Point
-        Point p = new Point(2,3,4, 6);
+        Point p1 = new Point(2,3);
 
-       System.out.println("Расстояние между точками R (" + p.x + ";" + p.y + ") и Т (" + p.ax + ";" + p.ay + ") равно " + (p.distance()));
+        Point p2 = new Point (4,5);
+
+       System.out.println("Расстояние между точками R (" + p1.x + ";" + p1.y + ") и Т (" + p2.x + ";" + p2.y + ") равно " + p1.distance(4, 5));
 
     }
 
