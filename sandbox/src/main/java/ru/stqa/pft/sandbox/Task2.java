8a9c93c6cc5094eb182b2d69666919aad9605cdd
MoskalenkoAndrey <alan3341106@gmail.com>
2018-12-11 14:43:59 +0300

    Правка 4 для задачи 3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Task2.java
@@ -5,9 +5,9 @@ public class Task2 {
 // Создание объектов Point
         Point p1 = new Point(2,3);
 
-        Point p2 = new Point (4,5);
+        Point p2 = new Point (10,12);
 
-       System.out.println("Расстояние между точками R (" + p1.x + ";" + p1.y + ") и Т (" + p2.x + ";" + p2.y + ") равно " + p1.distance(4, 5));
+       System.out.println("Расстояние между точками R (" + p1.x + ";" + p1.y + ") и Т (" + p2.x + ";" + p2.y + ") равно " + p1.distance(p2.x, p2.y));
 
     }
 
